1cf62c17ebdb9c6f2ec59e47cefd0c33f7edcb99
shkurkoka <44337947+shkurkoka@users.noreply.github.com>
2018-12-09 15:57:24 +0200

    Add files via upload

---
+++ click/script.js
@@ -0,0 +1,10 @@
+var aa = document.getElementById("btn1");
+var ab = document.getElementById("btn2");
+var ac = document.getElementById("btn3");
+var b = document.getElementById("block");
+aa.addEventListener("click", function(){
+    b.style.backgroundColor = "orange";
+});
+aa.addEventListener("click", function(){
+    b.style.backgroundColor = "orange";
+});
